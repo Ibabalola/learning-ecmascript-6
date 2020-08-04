af41af0a88f7790e183fe95341efbe6f143a951f
Isaac Babalola <info@isaacbabalola.co.uk>
2020-08-04 20:39:42 +0100

    adding desctructuring assignment

---
+++ src/destructuring-assignment.js
@@ -0,0 +1,34 @@
+/**
+ * Desctructuring assignment
+ * 
+ * Gives us an easy way to extract data from Arrays, Objects and asign them variables
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+ */
+
+ const [ first,,,fourth,,] = ["London", "Telford", "Birmingham", "Manchester", "Liverpool", "Newcastle"];
+
+ console.log(`first ${first}`);
+ console.log(`fourth ${fourth}`);
+
+ const {title, price} = { 
+     title: "Reuben",
+     price: 7,
+     description: "Cleveland's favourite sandwich",
+     ingredients: ['bread', 'corned beef', 'dressing', 'sauerkraut', 'cheese']
+ }
+
+ console.log(title);
+ console.log(price);
+
+ const vacation = {
+     destination: 'Chile',
+     travellers: 2,
+     activity: "skiing",
+     cost: 4000
+ }
+
+ function vacationMarkerting ({destination, activity}) {
+    return `Come to ${destination} and do some ${activity}`;
+ }
+
+ console.log(vacationMarkerting(vacation));
