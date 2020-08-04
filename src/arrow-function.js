e3412173a89604386282ea7e024c700906468644
Isaac Babalola <info@isaacbabalola.co.uk>
2020-08-04 20:20:39 +0100

    understanding of the this with arrow functions

--- src/arrow-function.js
+++ src/arrow-function.js
@@ -22,3 +22,38 @@ teams.map(team => console.log(`Club Name: ${team.name}`));
 teams.map(team => ({
     faCupWins: team.cups
 })).forEach(cup => console.log(`FA cup wins ${cup.faCupWins}`));
+
+/**
+ * Arrow function can help us deal with the scope of the this keyword
+ */
+
+ const person = {
+     first: 'Isaac',
+     actions: ['play', 'code', 'eat', 'sleep'],
+     printActions_with_this: function () {
+         const _this = this; // use this approach to help with the lost 
+         // of scope of the `this` with conventional function calls 
+
+         this.actions.forEach(function (action) {
+            const str = `${_this.first} likes to ${action}`;
+            console.log(str);
+        })
+     },
+     printActions_with_bind: function () {
+         this.actions.forEach(function (action) {
+            const str = `${this.first} likes to ${action}`;
+            console.log(str);
+        }.bind(this)); // .bind() helps with the lost of scope of the this
+     },
+     printActions_with_arrow: () => {
+        console.log(`his name is ${this.first}`); // undefined
+    },
+    printActions: function () {
+        this.actions.forEach(action => (
+            console.log('with parenthesis on return'),
+            console.log(`${this.first} likes to ${action}`)
+        ))
+    }
+ };
+
+ person.printActions();
